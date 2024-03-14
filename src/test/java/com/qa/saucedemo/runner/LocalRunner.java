40451b7e63680c71665fe4f346b4f9438216d230
AkashShitole1 <89238732+AkashShitole1@users.noreply.github.com>
2024-03-14 22:17:34 +0530

    Update LocalRunner.java

--- src/test/java/com/qa/saucedemo/runner/LocalRunner.java
+++ src/test/java/com/qa/saucedemo/runner/LocalRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = { "src\\test\\java\\com\\qa\\saucedemo\\features"}, glue = {
+@CucumberOptions(features = { "src/test/java/com/qa/saucedemo/features"}, glue = {
 		"com/qa/saucedemo/stepdefinitions","com/qa/hooks" }, plugin = { "pretty",
 				"html:reports/cucumber-default-report/cucumber.html", "json:reports/cucumber.json", "junit:reports/cucumber.xml" }, monochrome = true, dryRun = false, 
 				tags = "(@test1)"
